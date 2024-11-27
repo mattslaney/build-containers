4df7835342a5f063da34babe4fe612a1f38e99d1
mattslaney <mattslaney@users.noreply.github.com>
2024-11-27 18:03:05 +0000

    Make a bit more generic

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ if [ -z $PROJECT ]; then
 fi
 
 if [[ $PROJECT == "." ]]; then
-    echo "Building ubuntu-build-container"
-    podman build -t ubuntu-build-container .
+    echo "Building build-container"
+    podman build -t build-container .
     exit 0
 fi
 
-build_container="ubuntu-build-container"
+build_container="build-container"
 
 project_dir=$(pwd)/$PROJECT
 echo "Building $PROJECT: $project_dir"
@@ -24,11 +24,13 @@ if [ -f $project_dir/Containerfile ]; then
     podman build -f "$project_dir/Containerfile" -t $build_container
 fi
 
-if [ -d $project_dir/build ]; then
-    echo -e "\e[33mProject directory contains artifacts from a previous build\e[0m"
+if [ $(git ls-files --others -- $PROJECT | wc -l) -ne 0 ]; then
+    echo -e "\e[33mProject directory is unclean, do you want to clean it?"
+    git ls-files --others -- $PROJECT | awk '{print "    * " $0}'
     read -p "Do you want to delete them?" delete_artifacts
-    if [[ $delete_artifacts == "yes" ]]; then
-        rm -r $project_dir/build
+    echo -e -n "\e[0m"
+    if [[ $delete_artifacts =~ ^(yes|y)$ ]]; then
+        git clean -fdx $project_dir
     fi
 fi
 
