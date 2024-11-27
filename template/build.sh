3d7381c34fde01d395723539aeb9837deaaf187a
mattslaney <mattslaney@users.noreply.github.com>
2024-11-27 18:24:51 +0000

    Added template project for easier starting point

---
+++ template/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# A build.sh template
+
+if [ "$BUILD_CONTAINER" != "true" ]; then
+    echo "This should be run inside a build container"
+    exit 1
+fi
+
+echo "Inside build container"
+
+set -e
+
+mkdir -p /app/build     # Somewhere to put build artifacts
+mkdir -p /app/build/log # Somewhere to put build logs
+mkdir -p /app/dist      # Somewhere to put the release files
+
+cowsay "Well, hello there"
+
