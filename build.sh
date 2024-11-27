618dbbcbb81ac2db4ba9ad106421f34caf1d54b1
mattslaney <mattslaney@users.noreply.github.com>
2024-11-27 22:45:10 +0000

    Process args

--- build.sh
+++ build.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
+# This is the main build-container script used to build project subdirectories
 
+clean() {
+    local $PROJECT=$1
+    git clean -fdx $PROJECT
+}
+
+# Process first arg
 PROJECT=${1%/}
 if [ -z $PROJECT ]; then
     echo "Expected name of project to build"
     exit 1
 fi
 
+# We expect it to be a project directory
+if [ ! -d $PROJECT ]; then
+    echo "Expected first arg to be subdirectory (project)"
+    exit 1
+fi
+
+shift
+
+# Process remaining args
+while test $# -gt 0
+do
+    case "$1" in
+        --)         # echo "Got to --, next args are for project script"
+                    shift
+                    break
+                    ;;
+        --clean)    # echo "Cleaning $PROJECT"
+                    clean
+                    ;;
+        *)          echo -e "\e[33mUnexpected arg: $1\e[0m"
+                    ;;
+    esac
+    shift
+done
+
+# 
 if [[ $PROJECT == "." ]]; then
     echo "Building build-container"
     podman build -t build-container .
@@ -18,28 +51,35 @@ project_dir=$(pwd)/$PROJECT
 echo "Building $PROJECT: $project_dir"
 chmod +x $project_dir/build.sh
 
+# If the project has it's own Containerfile, use that
 if [ -f $project_dir/Containerfile ]; then
-    echo "Project contains own Containerfile, using this"
+    echo "Project contains own Containerfile, using that"
     build_container="$PROJECT-build-container"
     podman build -f "$project_dir/Containerfile" -t $build_container
 fi
 
-if [ $(git ls-files --others -- $PROJECT | wc -l) -ne 0 ]; then
-    echo -e "\e[33mProject directory is unclean, do you want to clean it?"
-    git ls-files --others -- $PROJECT | awk '{print "    * " $0}'
-    read -p "Do you want to delete them?" delete_artifacts
-    echo -e -n "\e[0m"
-    if [[ $delete_artifacts =~ ^(yes|y)$ ]]; then
-        git clean -fdx $project_dir
+# Before running check if there are existing build artifacts - unclean tree
+# Unless it's a new (untracked) project dir
+if git ls-files --error-unmatch $PROJECT &>/dev/null; then
+    if [ $(git ls-files --others -- $PROJECT | wc -l) -ne 0 ]; then
+        echo -e "\e[33mProject directory is unclean, do you want to clean it?"
+        git ls-files --others -- $PROJECT | awk '{print "    * " $0}'
+        read -p "Do you want to delete them?" delete_artifacts
+        echo -e -n "\e[0m"
+        if [[ $delete_artifacts =~ ^(yes|y)$ ]]; then
+            clean $PROJECT
+        fi
     fi
 fi
 
 echo "Starting build in $build_container"
+echo -e "\e[34m"
 if [ "$DEBUG" = "true" ]; then
-    echo "Starting container for debug"
-    podman run --rm -it -v $project_dir:/app $build_container /bin/bash
+    podman run --rm -it -v $project_dir:/app -e ARGS="$@" $build_container /bin/bash
 else
-    echo "Running app build"
-    podman run --rm -v $project_dir:/app $build_container
+    podman run --rm -v $project_dir:/app $build_container /app/build.sh "$@"
 fi
+echo -e "\e[0m"
+
+echo "Build finished"
 
--- hello/build.sh
+++ hello/build.sh
@@ -10,17 +10,25 @@ echo "Inside build container"
 
 set -e
 
+mkdir -p /app/build/log
+mkdir -p /app/dist
+
+USER="World"
+if [ -n "$1" ]; then
+    echo "Using custom user: $1"
+    USER=$1
+fi
+
 mkdir -p /usr/src/hello
 cat > /usr/src/hello/hello.cpp << EOF
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, $USER!" << std::endl;
     return 0;
 }
 EOF
 
-mkdir -p /app/build/log
 g++ -v /usr/src/hello/hello.cpp -o /usr/src/hello/hello 2>&1 | tee /app/build/log/build.log
 result=${PIPESTATUS[0]}
 if [ $result -ne 0 ]; then
@@ -33,6 +41,5 @@ if [ ! -f /usr/src/hello/hello ]; then
     exit 1
 fi
 
-mkdir -p /app/build/release
-cp /usr/src/hello/hello /app/build/release
+cp /usr/src/hello/hello /app/dist
 
